7e03e3d547b9048da07362a2d33689c1910e7bca
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2023-04-18 14:39:06 -0300

    creating relationships between models
    aqui foi criada a relação entre os nossos modelos do banco de dados

---
+++ App-Barbearia/ws/src/models/relationship/salaoColaborador.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const salaoColaborador = new Schema({
+    salaoId:{
+        type: mongoose.Types.ObjectId,
+        ref: 'Salao',
+        required: true,
+    },
+    colaboradorId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Colaborador',
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['A', 'I'],
+        default: 'A',
+    },
+    dataCadastro: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('SalaoColaborador', salaoColaborador);
