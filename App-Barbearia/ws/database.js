2f069ef2a0a6fa956b5fbec2d751570cdd3a4b34
João Pedro ⚡ <joaopedrobraga.07@gmail.com>
2023-04-18 16:23:49 -0300

    Create database connection
    Aqui foi criada a conexão com o banco de dados MongoDB

--- App-Barbearia/ws/database.js
+++ App-Barbearia/ws/database.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
-const URL = '';
+const server = 'mongodb+srv://jpdicarvalho:joaopedro14165@cluster-app-barbearia.ldlclfr.mongodb.net/?retryWrites=true&w=majority';
+const database = 'myDatabase';
 
-mongoose.set('useNewUrlParser', true);
+mongoose.connect(`${server}/${database}`,
+   { useNewUrlParser: true, useUnifiedTopology: true }
+).then(() => {
+   console.log("Banco de Dados MongoDB conectado.");
+})
+   .catch(err => {
+      console.error("Erro na conexão com o Banco de Dados", err);
+      process.exit();
+   });
+
+/*mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
@@ -9,4 +20,4 @@ mongoose.set('useUnifiedTopology', true);
 mongoose
     .connect(URL)
     .then(() => console.log('DB is Up!'))
-    .catch(() => console.log(err));
+    .catch(() => console.log('deu erro'));*/
--- App-Barbearia/ws/index.js
+++ App-Barbearia/ws/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+require('./database');
 
 app.use(morgan('dev'));
 
